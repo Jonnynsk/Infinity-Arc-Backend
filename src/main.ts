a107c86a29bcc5b807309cfd30e5f32c73326db0
Jonnynsk <@email.com>
2025-11-12 18:54:13 +0700

    feat: config

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { NestFactory } from "@nestjs/core";
-import { ValidationPipe } from "@nestjs/common";
+import { Logger, ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
 import { AppModule } from "./app.module";
+
+import { getCorsConfig } from "./config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +13,21 @@ async function bootstrap() {
 
   app.setGlobalPrefix("api");
 
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+  const logger = new Logger(AppModule.name);
+
+  app.enableCors(getCorsConfig(config));
+
+  const port = config.getOrThrow<number>("HTTP_PORT");
+  const host = config.getOrThrow<string>("HTTP_HOST");
+
+  try {
+    await app.listen(port);
+
+    logger.log(`Server is running on: ${host}`);
+  } catch (error) {
+    logger.error("Failed to start server: ${error.message}", error);
+    process.exit(1);
+  }
 }
 bootstrap();
